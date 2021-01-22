3c2d5a108e4d00902ed91995b37d210aefdda3e9
Graham Hoyes <accounts@grahamhoyes.com>
2021-01-22 17:16:45 -0500

    Connection class and sendMessage method

---
+++ src/shared/Connection.java
@@ -0,0 +1,72 @@
+package shared;
+
+import org.apache.log4j.Logger;
+import shared.messages.KVMessage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+/*
+ * Abstract connection class with methods that are common to both
+ * the client and server for communications purpose.
+ */
+public abstract class Connection {
+    private static final Logger logger = Logger.getRootLogger();
+
+    // Header size in bytes
+    private static final int HEADER_SIZE = 16;
+    private static final byte PROTOCOL_VERSION = 1;
+
+    private Socket socket;
+    private InputStream input;
+    private OutputStream output;
+
+    /**
+     * Send a message using this connection's socket.
+     *
+     * Serialized message strings are decoded and sent as bytes. The packet
+     * begins with a 16-byte header. The first byte indicates the protocol
+     * version (here, 1). The next 4 bytes indicate the size of the message
+     * body (little endian). The remaining 11 bytes are currently unused.
+     *
+     * @param message The message to send
+     */
+    public void sendMessage(KVMessage message) throws IOException {
+        String msgContent = message.serialize();
+        byte[] rawMsgBytes = msgContent.getBytes();
+
+        ByteBuffer msgBuffer = ByteBuffer.allocate(rawMsgBytes.length + HEADER_SIZE);
+        msgBuffer.order(ByteOrder.LITTLE_ENDIAN);
+
+        // Add the 16-byte header to the beginning of the message
+        msgBuffer.put(PROTOCOL_VERSION);
+        msgBuffer.putInt(rawMsgBytes.length);
+
+        for (int i = msgBuffer.position(); i < HEADER_SIZE; i++) {
+            msgBuffer.put((byte) 0);
+        }
+
+        // Add the rest of the message
+        msgBuffer.put(rawMsgBytes, 0, rawMsgBytes.length);
+
+        byte[] msgBytes = msgBuffer.array();
+
+        // Send the message
+        output.write(msgBytes, 0, msgBytes.length);
+        output.flush();
+
+        logger.info("SEND \t"
+                + socket.getInetAddress().getHostName() + ":"
+                + socket.getPort() + ">: '"
+                + msgContent + "'");
+    }
+
+    public void receiveMessage() {
+
+    }
+
+}
