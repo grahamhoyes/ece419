b167b68e4b153e1de6a28aec8baebd3c4bd89b0f
Danial <danialshasan@gmail.com>
2021-01-27 17:58:12 -0500

    removed extraneous exceptions

--- src/store/exceptions/GetKeyInvalidException.java
+++
@@ -1,7 +0,0 @@
-package store.exceptions;
-
-public class GetKeyInvalidException extends Exception{
-    public GetKeyInvalidException(String key) {
-        super("Invalid key used: " + key);
-    }
-}
--- src/store/exceptions/PutKeyInvalidException.java
+++
@@ -1,7 +0,0 @@
-package store.exceptions;
-
-public class PutKeyInvalidException extends Exception{
-    public PutKeyInvalidException(String key) {
-        super("Invalid key used for deletion: " + key);
-    }
-}
