fc68116a566cb07089757039d19c42d0eebc3f7d
Danial <danialshasan@gmail.com>
2021-02-23 22:01:38 -0500

    working on ECS tests

--- src/testing/AllTests.java
+++ src/testing/AllTests.java
@@ -11,11 +11,12 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-		ConnectionTest.class,
-		InteractionTest.class,
-		JsonMessageTest.class,
-		KVStoreTest.class,
-		CommunicationProtocolTest.class
+//		ConnectionTest.class,
+//		InteractionTest.class,
+//		JsonMessageTest.class,
+//		KVStoreTest.class,
+//		CommunicationProtocolTest.class,
+		ECSTest.class
 })
 public class AllTests {
 	// TODO: Tests for ZooKeeper
@@ -23,10 +24,10 @@ public class AllTests {
 	@BeforeClass
 	public static void setUp() {
 		try {
-			new LogSetup("logs/testing/test.log", Level.ERROR);
-			KVServer server = new KVServer(50000, "127.0.0.1:50000", "127.0.0.1", 2181);
-			server.clearStorage();
-			new Thread(server).start();
+			new LogSetup("logs/testing/test.log", Level.ALL);
+//			KVServer server = new KVServer(50000, "127.0.0.1:50000", "127.0.0.1", 2181);
+//			server.clearStorage();
+//			new Thread(server).start();
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.exit(1);
